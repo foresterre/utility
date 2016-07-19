a15cee296371f402e50652d1fdeafe69f8152414
Marwen Martino <foresterre@ilumeo.com>
2016-07-19 20:22:35 +0200

    ensure path can be found in powershell

--- scripts/fs/windows_delete_longpath.cmd
+++ scripts/fs/windows_delete_longpath.cmd
@@ -1,4 +1,4 @@
 cd /
-mkdir empty
-robocopy "/empty" %1 /purge
-rmdir empty
+if not exist "%appdata%/temp/empty" mkdir "%appdata%/temp/empty"
+robocopy "%appdata%/temp/empty" %1 /purge
+rmdir "%appdata%/temp/empty"
